bceef2588ab8fc58748de46a29c2896c311cd1b0
alex-at-drom <138438573+alex-at-drom@users.noreply.github.com>
2024-01-15 17:42:10 +0100

    Update config/index.ts
    Modify APPInfo

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
+  title: 'DROM Text Generator APP',
+  description: 'This is the frontend of DROMs amazing AI-enabled APP!!1!',
+  copyright: '2024',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
